40f8109b131d54cb3cf39577531c824e65521116
Suhnea <SangoVai@mail.ru>
2026-03-23 22:08:22 +0700

    подчистила файлы и поменяла контакты

--- php/api/get_applications.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: application/json; charset=utf-8');
-$file = '../data/applications.json';
-$apps = file_exists($file) ? json_decode(file_get_contents($file), true) : [];
-echo json_encode($apps);
-?>
